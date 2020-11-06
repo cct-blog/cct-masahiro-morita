6cf6753d0f78d20883b38702f4f8dcfa2fc816d1
masahiro.morita <masahiro.morita@cct-inc.co.jp>
2020-11-06 19:19:40 +0900

    Add: add PostService Method

---
+++ Server/Services/PostService.cs
@@ -0,0 +1,34 @@
+﻿using blazorTest.Server.Data;
+using blazorTest.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorTest.Server.Services
+{
+    public class PostService
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public PostService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Post> ReadPostWhenWindowOpened(Guid roomId, string userId)
+        {
+            var lastAccesseDate = _context.UserInfoInRooms
+                .Single(_userInfoInRooms => _userInfoInRooms.UserId.Equals(userId) && _userInfoInRooms.RoomId.Equals(roomId))
+                .LatestAccessDate;
+
+            var posts = _context.Posts
+                .Where(_post => _post.RoomId.Equals(roomId))
+                .Where(_post => _post.CreateDate > lastAccesseDate)
+                .ToList();
+
+            return posts
+        }
+    }
+}
---
+++ blazorTest.ServerTests/Services/PostServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using blazorTest.Server.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework.Internal;
+
+namespace blazorTest.Server.Services.Tests
+{
+    [TestClass()]
+    public class PostServiceTests
+    {
+        [TestMethod()]
+        public void ReadPostWhenWindowOpenedTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
