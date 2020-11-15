579f40cfa56721e66ce888656a3a8ac3bd84f922
Masahiro Morita <nnndrum@yahoo.co.jp>
2020-11-15 15:38:22 +0900

    Update: save message input to db(roomId is dummy, need to change)

--- Server/Hubs/ChatHub.cs
+++ Server/Hubs/ChatHub.cs
@@ -2,7 +2,6 @@
 using blazorTest.Server.Models;
 using Microsoft.AspNetCore.SignalR;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,10 +9,21 @@ namespace blazorTest.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
-        {   
+        private readonly ApplicationDbContext _context;
 
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        public ChatHub(ApplicationDbContext context) => _context = context;
+
+        public async Task SendMessage(string email, string message, Guid roomId)
+        {
+            var user = _context.Users
+                .Where(user => user.Email == email)
+                .Single();
+
+            var post = new Post() { ApplicationUserId = user.Id, Text = message, RoomId = roomId };
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+
+            await Clients.All.SendAsync("ReceiveMessage", user.HandleName, email, message);
         }
     }
 }
