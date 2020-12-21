30ffc9a2ccb5c1138703d8df2d8115ba6b562d6a
masahiro.morita <masahiro.morita@cct-inc.co.jp>
2020-12-21 19:00:05 +0900

    Add:add response model of getting room detail API

---
+++ Shared/Models/RoomDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blazorTest.Shared.Models
+{
+    public record RoomDetail
+    {
+        public Guid RoomId { get; init; }
+
+        public string RoomName { get; init; }
+
+        public DateTime CreateDate { get; init; }
+
+        public DateTime UpdateDate { get; init; }
+
+        public DateTime LastAccessedDate { get; init; }
+
+        public List<Guid> UserId { get; init; }
+    }
+}
