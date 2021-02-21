3fda10004d084dd3c4aec8328f11bec41a36a837
nenaaki <nenaaki@gmail.com>
2021-02-21 17:43:22 +0900

    ユーザー一覧コンポーネント

---
+++ Client/Shared/Components/SelectableContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorTest.Client.Shared.Components
+{
+    public class SelectableContext<TItem>
+    {
+        public bool IsSelected { get; set; }
+
+        public bool IsEnabled { get; set; }
+
+        public TItem Value { get; set; }
+    }
+}
