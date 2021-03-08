8ffd3b4a4d64fb0e216d7a124aca1509d309e236
masahiro.morita <masahiro.morita@cct-inc.co.jp>
2021-03-08 19:28:28 +0900

    Update: refactoring ChatHub

--- Server/Hubs/ChatHub.cs
+++ Server/Hubs/ChatHub.cs
@@ -3,6 +3,7 @@ using blazorTest.Server.Models;
 using blazorTest.Shared;
 using blazorTest.Shared.Models;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,9 +18,16 @@ namespace blazorTest.Server.Hubs
 
         public async Task SendMessage(Message message)
         {
-            var user = _context.Users
-                .Where(user => user.Email == message.UserEmail)
-                .Single();
+            var user = await _context.Users
+                .FirstOrDefaultAsync(user => user.Email == message.UserEmail);
+
+            if (user is null) throw new HubException("User is not Exsisted");
+
+            if (!await _context.Rooms
+                .AnyAsync(room => room.Id == message.RoomId))
+            {
+                throw new HubException("Specified room is not exsisted");
+            }
 
             var post = new Post()
             {
@@ -29,7 +37,7 @@ namespace blazorTest.Server.Hubs
                 RoomId = message.RoomId
             };
             _context.Posts.Add(post);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             await Clients.All.SendAsync(SignalRMehod.ReceiveMessage,
                 new Message
