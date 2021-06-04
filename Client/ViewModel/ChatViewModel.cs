75d89ce98e8942ffcba78763c7ebef07de907ce0
nenaaki <nenaaki@gmail.com>
2021-06-04 15:42:18 +0900

    ViewModelを追加

---
+++ Client/ViewModel/ChatViewModel.cs
@@ -0,0 +1,18 @@
+﻿using blazorTest.Client.Models;
+using Oniqys.Blazor.ViewModel;
+
+namespace blazorTest.Client.ViewModel
+{
+    public class ChatViewModel : ContentBase
+    {
+        private ChatModel _model = new();
+
+        /// <summary>
+        /// 全ユーザーと（入室中情報付き）
+        /// </summary>
+        public ContentCollection<Selectable<UserViewModel>> Users { get; set; } = new();
+
+        public Selectable UserListOpened { get; } = new Selectable { IsSelected = false, IsEnabled = true };
+
+    }
+}
---
+++ Client/ViewModel/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Oniqys.Blazor.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorTest.Client.ViewModel
+{
+    public class UserViewModel : ContentBase
+    {
+        public string Id { get; init; }
+
+        public string Name { get; set; }
+    }
+}
