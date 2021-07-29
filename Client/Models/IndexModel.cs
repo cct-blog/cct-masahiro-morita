a24da126fd60644ac5bc9ef7458e6b121d6a58d6
masahiro.morita <masahiro.morita@cct-inc.co.jp>
2021-07-29 20:41:43 +0900

    Update: Create Index page model

---
+++ Client/Models/IndexModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using ChatApp.Shared.Models;
+
+namespace ChatApp.Client.Models
+{
+    public class IndexModel
+    {
+        private readonly HttpClient _httpClient;
+
+        public List<RoomModel> RoomModels { get; set; } = new List<RoomModel>();
+
+        public event EventHandler<List<RoomModel>> RoomListChanged;
+
+        public IndexModel(IHttpClientFactory _httpClientFactory)
+        {
+            _httpClient = _httpClientFactory.CreateClient("ChatApp.ServerAPI");
+        }
+
+        public async Task GetUserBelongedRoomsAsync()
+        {
+            var rooms = await _httpClient.GetFromJsonAsync<List<UserRoom>>("Room");
+
+            RoomModels = rooms
+                .Select((room) =>
+                    new RoomModel()
+                    {
+                        RoomId = room.Id,
+                        RoomName = room.Name,
+                        LastAccessDate = room.LastAccessDate
+                    })
+                .ToList();
+
+            RoomListChanged?.Invoke(this, RoomModels);
+        }
+
+        public async Task CreateRoomAsync(string roomName, List<string> userEmails)
+        {
+            await _httpClient.PostAsJsonAsync(
+                "Room",
+                new CreateRoom() { RoomName = roomName, UserIds = userEmails });
+
+            await GetUserBelongedRoomsAsync();
+        }
+    }
+}
