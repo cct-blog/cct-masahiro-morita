16d902f0e75b991f5e543d2f0b17be0ff3b85016
masahiro.morita <masahiro.morita@cct-inc.co.jp>
2021-08-13 20:14:10 +0900

    Update: create ChatModel

--- Client/Models/PostModel.cs
+++ Client/Models/PostModel.cs
@@ -1,4 +1,5 @@
 ﻿using ChatApp.Shared.Models;
+using Microsoft.AspNetCore.SignalR.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +9,8 @@ using System.Threading.Tasks;
 
 namespace ChatApp.Client.Models
 {
-    public delegate void ChangePostMessage();
-
-    public delegate void ChangeThreadCountEventHandler();
-
     public class PostModel : ICreateAndUpdateDate
     {
-
         public List<ThreadModel> ThreadModels { get; set; }
 
         public string UserEmail { get; set; }
@@ -28,47 +24,5 @@ namespace ChatApp.Client.Models
         public DateTime CreateDate { get; set; }
 
         public DateTime UpdateDate { get; set; }
-
-        public event ChangePostMessage ChangePost;
-
-        public event ChangeThreadCountEventHandler ChangeThread;
-
-        public async Task ChangeMessage(ChatPostUpdateRequest request, HttpClient httpClient)
-        {
-            await httpClient.PutAsJsonAsync($"Post/{PostId}", request);
-
-            MessageContext = request.Text;
-
-            ChangePost();
-        }
-
-        public async Task SendThreadMessage(ThreadMessage message, HttpClient httpClient)
-        {
-            var response = await httpClient.PostAsJsonAsync("Post", message);
-
-            var threadMessage = await response.Content.ReadFromJsonAsync<ThreadMessage>();
-
-            ThreadModels.Add(new ThreadModel()
-            {
-                UserEmail = threadMessage.UserEmail,
-                HandleName = threadMessage.HandleName,
-                ThreadId = threadMessage.ThreadId,
-                MessageContext = threadMessage.MessageContext,
-                CreateDate = threadMessage.CreateDate
-            });
-
-            ChangeThread();
-        }
-
-        public async Task DeleteThreadMessage(Guid threadId, HttpClient httpClient)
-        {
-            await httpClient.DeleteAsync($"Thread/{threadId}");
-
-            var deletedThread = ThreadModels.FirstOrDefault(thread => thread.ThreadId == threadId);
-            ThreadModels.Remove(deletedThread);
-
-            ChangeThread();
-        }
-
     }
 }
